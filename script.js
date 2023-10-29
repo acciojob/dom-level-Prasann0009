becc3f2d48d3c0b6484b490cdb80e090d93d944c
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2023-10-29 14:00:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const targetElement = document.getElementById('level');
+
+
+function getDomLevel(element) {
+    let level = 0;
+    let currentElement = element;
+
+    while (currentElement !== document.documentElement) {
+        currentElement = currentElement.parentNode;
+        level++;
+    }
+
+    return level;
+}
+
+
+const domLevel = getDomLevel(targetElement);
+
+alert(`The level of the element is: ${domLevel}`);
+
